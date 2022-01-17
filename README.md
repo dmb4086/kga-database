ef75de169fa2af79f0ced3278ad811a74821654f
Ethan Ruszanowski <ethan@rusz.dev>
2022-01-16 22:29:31 -0500

    Create Python3 test skeleton

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # kga-database
 
 ## Create and Test Database
+
 Instructions to create and test the database are located in `kga-database/scripts`.
--- scripts/create_kga.sql
+++ scripts/create_kga.sql
@@ -1,5 +1,12 @@
 -- Create a test table to validate creation method
-CREATE TABLE test (
-	test_id INTEGER PRIMARY KEY,
-	test_name TEXT NOT NULL
+CREATE TABLE test
+(
+    test_id   INTEGER PRIMARY KEY,
+    test_name TEXT NOT NULL
 );
+
+-- Some test data to read
+INSERT INTO test (test_name)
+VALUES ('A Test');
+INSERT INTO test (test_name)
+VALUES ('Another Test');
