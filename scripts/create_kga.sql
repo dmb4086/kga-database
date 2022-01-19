7d1fcb6e9f1beaf9fc2913295cbc089762bfe132
Ethan Ruszanowski <ethan@rusz.dev>
2022-01-19 15:27:25 -0500

    Add some tentative create tables

--- scripts/create_kga.sql
+++ scripts/create_kga.sql
@@ -10,3 +10,30 @@ INSERT INTO test (test_name)
 VALUES ('A Test');
 INSERT INTO test (test_name)
 VALUES ('Another Test');
+
+-- Create plant type
+CREATE TABLE plant_type (
+    id INTEGER PRIMARY KEY,
+    type TEXT NOT NULL
+);
+
+-- Add material table
+CREATE TABLE material (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL
+);
+
+-- Add required tables for ailment
+-- symptom
+CREATE TABLE symptom (
+    id INTEGER PRIMARY KEY,
+    symptom TEXT NOT NULL,
+    description TEXT NOT NULL
+);
+
+CREATE TABLE remedy (
+    id INTEGER PRIMARY KEY,
+    symptom TEXT NOT NULL,
+    description TEXT NOT NULL
+);
