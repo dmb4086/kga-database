6e006cc6f4147ccf8065429579c6124b9da40bc8
Ethan Ruszanowski <ethan@rusz.dev>
2022-01-21 20:25:21 -0500

    Update scripts

--- scripts/create_kga.sql
+++ scripts/create_kga.sql
@@ -1,16 +1,3 @@
--- Create a test table to validate creation method
-CREATE TABLE Test
-(
-    test_id   INTEGER PRIMARY KEY,
-    test_name TEXT NOT NULL
-);
-
--- Some test data to read
-INSERT INTO Test (test_name)
-VALUES ('A Test');
-INSERT INTO Test (test_name)
-VALUES ('Another Test');
-
 -- Create tables for plants
 CREATE TABLE Symptom
 (
@@ -71,18 +58,18 @@ CREATE TABLE Project
 
 CREATE TABLE LifeCycle
 (
-    state_id  INTEGER PRIMARY KEY,
+    state_id    INTEGER PRIMARY KEY,
     name        TEXT NOT NULL,
     description TEXT
 );
 
 CREATE TABLE Plant
 (
-    plant_id   INTEGER PRIMARY KEY,
+    plant_id     INTEGER PRIMARY KEY,
     planted_date DATE,
-    project_id INTEGER NOT NULL,
-    type_id    INTEGER NOT NULL,
-    state_id   INTEGER NOT NULL,
+    project_id   INTEGER NOT NULL,
+    type_id      INTEGER NOT NULL,
+    state_id     INTEGER NOT NULL,
     CONSTRAINT fk_project
         FOREIGN KEY (project_id)
             REFERENCES Project (project_id),
@@ -93,3 +80,55 @@ CREATE TABLE Plant
         FOREIGN KEY (state_id)
             REFERENCES LifeCycle (state_id)
 );
+
+-- Add some symptoms
+INSERT INTO Symptom (symptom_id, name, description)
+VALUES (1, 'Dark Spots', 'Dark spots with concentric rings develop on older leaves first that touch infected soil.');
+
+-- Add some remedies
+INSERT INTO Remedy (remedy_id, name, description)
+VALUES (1, 'Remove', 'Remove affected plants and thoroughly clean fall garden debris. Do not compost affected plants.');
+
+-- Add some ailments
+INSERT INTO Ailment (ailment_id, name, remedy_id, symptom_id)
+VALUES (1, 'Early Blight', 1, 1);
+
+-- Add materials
+INSERT INTO Material (material_id, name, description)
+VALUES (1, 'Trowel', 'A small handheld shovel or spade.');
+INSERT INTO Material (material_id, name, description)
+VALUES (2, 'Pruning Shears', 'Hand pruners, also called secateurs, help reign in plants that are getting out of control and taking over.');
+INSERT INTO Material (material_id, name, description)
+VALUES (3, 'Watering Can', 'There are 2 basic types of watering cans, plastic or metal. There are hundreds of styles, colors, sizes and nozzle options.');
+
+-- Add types
+INSERT INTO PlantType (type_id, name, description, ailment_id, material_id)
+VALUES (1, 'Tomato', 'Some kind of fruit, eh.', 1, 1);
+INSERT INTO PlantType (type_id, name, description, ailment_id, material_id)
+VALUES (2, 'Pepper', 'Another kind of fruit, eh.', 1, 2);
+INSERT INTO PlantType (type_id, name, description, ailment_id, material_id)
+VALUES (3, 'Eggplant', 'The eggplant is a delicate, tropical perennial plant often cultivated as a tender or half-hardy annual in temperate climates.', 1, 3);
+
+-- Add projects
+INSERT INTO Project (project_id, name, description)
+VALUES (1, 'Project One', 'This is project one');
+INSERT INTO Project (project_id, name, description)
+VALUES (2, 'Project Two', 'This is project two');
+INSERT INTO Project (project_id, name, description)
+VALUES (3, 'Project Three', 'This is project three');
+
+-- Add lifecycle states
+INSERT INTO LifeCycle (state_id, name)
+VALUES (1, 'Germination');
+INSERT INTO LifeCycle (state_id, name)
+VALUES (2, 'Seedling');
+INSERT INTO LifeCycle (state_id, name)
+VALUES (3, 'Adult Plant');
+
+-- Add some plants
+INSERT INTO Plant (plant_id, planted_date, project_id, type_id, state_id) 
+VALUES (1, 20220121, 1, 1, 3);
+INSERT INTO Plant (plant_id, planted_date, project_id, type_id, state_id)
+VALUES (2, 20220121, 2, 2, 2);
+INSERT INTO Plant (plant_id, planted_date, project_id, type_id, state_id)
+VALUES (3, 20220121, 3, 3, 1);
--- scripts/database_test.py
+++ scripts/database_test.py
@@ -19,14 +19,68 @@ def create_connection(database_file) -> sqlite3.Connection:
     return connection
 
 
-def select_test(conn) -> None:
-    """Temporary test to validate python connection to SQLite3 DB
-    FIXME: Remove this from final script.
-    :param conn: connection object
+def type_by_project(conn, project_number) -> None:
+    """Prints the name and state of all plants in a project.
+
+    :param conn: Connection object
+    :param project_number: Int, project to query
     :return: None
     """
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM test")
+    cursor.execute(
+        "SELECT PR.name, PlantType.name, LC.name "
+        "FROM PlantType "
+        "JOIN Plant PL ON PlantType.type_id = PL.type_id "
+        "JOIN LifeCycle LC on PL.state_id = LC.state_id "
+        "JOIN Project PR on PL.project_id = PR.project_id "
+        "WHERE PL.project_id = ?",
+        (project_number,)
+    )
+
+    results = cursor.fetchall()
+
+    for result in results:
+        print(result)
+
+
+def materials_by_plant(conn, plant_name) -> None:
+    """Prints name and description of materials needed by plant
+
+    :param conn: Connection object
+    :param plant_name: str, name of plant
+    :return: None
+    """
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT PT.name, Material.name, Material.description "
+        "FROM Material "
+        "JOIN PlantType PT ON Material.material_id = PT.material_id "
+        "WHERE PT.name LIKE ?",
+        (plant_name,)
+    )
+
+    results = cursor.fetchall()
+
+    for result in results:
+        print(result)
+
+
+def ailment_by_name(conn, ailment_name) -> None:
+    """Prints symptoms and remedies by ailment name
+
+    :param conn: Connection object
+    :param ailment_name: str, name of the ailment to search
+    :return: None
+    """
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT Ailment.name, S.name, S.description, R.name, R.description "
+        "FROM Ailment "
+        "JOIN Symptom S ON S.symptom_id = Ailment.symptom_id "
+        "JOIN Remedy R ON R.remedy_id = Ailment.remedy_id "
+        "WHERE Ailment.name LIKE ?",
+        (ailment_name,)
+    )
 
     results = cursor.fetchall()
 
@@ -46,10 +100,27 @@ def main():
 
     connection = create_connection(sys.argv[1])  # Create connection object
 
+    print("===== Example 1 =====")
     with connection:
-        # Test connection to db FIXME: Remove
-        print('Selecting from test:')
-        select_test(connection)
+        print("Prints all plant types in a project and their state.\n"
+              "(Joins PlantType, Plant, LifeCycle, Project)")
+        for project in range(1, 4):
+            print("Project", project)
+            type_by_project(connection, project)
+
+    print("\n===== Example 2 =====")
+    with connection:
+        print("Show tools you will need by plant\n"
+              "(Join Material and PlantType")
+        plant_names = ['tomato', 'eggplant', 'pepper']
+        for name in plant_names:
+            materials_by_plant(connection, name)
+
+    print("\n===== Example 3 =====")
+    with connection:
+        print("Show symptoms and remedies by ailment\n"
+              "(Join Ailment, Symptom, and Remedy)")
+        ailment_by_name(connection, "early blight")
 
 
 if __name__ == '__main__':
